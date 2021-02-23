3bc4c317cf453116425098a80e562b9644f38b73
Dzianis Kachanau <DzianisKachanau@ibagroup.eu>
2021-02-23 11:59:57 +0300

    Remove Duplicates from Sorted Array

---
+++ src/main/java/arrays/remove/element/Solution.java
@@ -0,0 +1,48 @@
+package arrays.remove.element;
+
+import java.util.Arrays;
+
+/* Remove Duplicates from Sorted Array */
+public class Solution {
+  public int removeElement(int[] nums, int val) {
+    int pos = 0, size = nums.length;
+
+    while (pos < size) {
+      while (nums[pos] == val && pos < size) {
+        nums[pos] = nums[size - 1];
+        size--;
+      }
+      pos++;
+    }
+
+    return size;
+  }
+}
+
+class TestItem {
+  public int[] arr;
+  public int val;
+
+  TestItem(int[] arr, int val) {
+    this.arr = arr;
+    this.val = val;
+  }
+}
+
+class Main {
+  private static final TestItem[] tests = {
+      new TestItem(new int[]{3, 2, 2, 3}, 3),
+      new TestItem(new int[]{0, 1, 2, 2, 3, 0, 4, 2}, 2),
+      new TestItem(new int[]{3, 3}, 3),
+  };
+
+  public static void main(String[] args) {
+    Solution sol = new Solution();
+
+    for (TestItem test : tests) {
+      int res = sol.removeElement(test.arr, test.val);
+
+      System.out.printf("%s %d\n", Arrays.toString(test.arr), res);
+    }
+  }
+}
